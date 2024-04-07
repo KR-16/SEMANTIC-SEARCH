b1d7f2771c426b74e8efb35002e68f6fdb6f5dc9
KEERTHI RAJ <73049486+KR-16@users.noreply.github.com>
2024-04-06 21:35:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,11 +17,17 @@ st.title("SEMANTIC SEARCH")
 input = st.text_input("Enter the query to search")
 k = st.number_input("Enter the top nearest values to be displayed", min_value=1, max_value=1000, step=1, format="%d")
 model = SentenceTransformer("paraphrase-distilroberta-base-v1")
-if input and k:
-    user_embedding = model.encode([input])[0]
-    index = embedding_generator()
-    D, I = index.search(np.array([user_embedding.astype("float32")]), k)
-    data = DataPreprocessor("imdb_top_1000.csv")
-    st.write(f"Top {k} closest values from the search")
-    for i in I[0]:
-        st.write(data.output().iloc[i])
+
+start = st.button("Start")
+
+if start:
+    if not input or not k:
+        st.error("Enter the query and the k value")
+    else:
+        user_embedding = model.encode([input])[0]
+        data, index = embedding_generator()
+        D, I = index.search(np.array([user_embedding.astype("float32")]), k)
+        # data = DataPreprocessor("imdb_top_1000.csv")
+        st.write(f"Top {k} closest values from the search")
+        for i in I[0]:
+            st.write(data[["Series_Title", "Runtime", "Genre", "IMDB_Rating",	"Overview"]].iloc[i])
