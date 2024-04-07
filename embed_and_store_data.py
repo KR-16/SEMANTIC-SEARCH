e1e34ed8c45b67a37e70f982daef152b4ce6b246
KEERTHI RAJ <73049486+KR-16@users.noreply.github.com>
2024-04-06 21:36:17 -0500

    Update embed_and_store_data.py

--- embed_and_store_data.py
+++ embed_and_store_data.py
@@ -14,9 +14,10 @@ import faiss
 def embedding_generator():
     model = SentenceTransformer("paraphrase-distilroberta-base-v1")
     processing = DataPreprocessor("imdb_top_1000.csv")
-    text = processing.concatenate_text_columns()
+    text = processing.text_column()
+    data = processing.output()
     embeddings = model.encode(text.tolist())
     # print(embeddings.shape) - 1000, 768 (1000 rows and 768 dimensions)
     index = faiss.IndexFlatIP(embeddings.shape[1])
     index.add(embeddings.astype("float32"))
-    return index
+    return data, index
